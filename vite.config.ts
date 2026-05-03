8f1c2356bf2a2e332238959da8e1471e73688270
jj121 <office.tenarch@gmail.com>
2026-05-03 09:29:43 +0300

    fix: strictPort=true to prevent silent port drift in dev server

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 7000,
-    strictPort: false,
+    strictPort: true,
     hmr: {
       protocol: 'ws',
       host: 'localhost',
